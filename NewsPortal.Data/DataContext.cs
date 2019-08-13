c35a41e21c7a3824dfefb554846ce80bdae0890d
Ptize <golubevivan-99@mail.ru>
2019-08-13 11:40:34 +0300

    Добавил Swagger

---
+++ NewsPortal.Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPortal.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
