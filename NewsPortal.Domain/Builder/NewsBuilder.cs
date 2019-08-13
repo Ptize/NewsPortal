cf79d9042cb79bf3b4344c109d6ad3bc67b07451
Ptize <golubevivan-99@mail.ru>
2019-08-13 17:26:00 +0300

    Создал контроллер
    Создал контроллер. Основные функции заработают как только я подключу БД.

---
+++ NewsPortal.Domain/Builder/NewsBuilder.cs
@@ -0,0 +1,52 @@
+﻿using NewsPortal.Data.interfaces;
+using NewsPortal.Domain.Storage.Interfaces;
+using NewsPortal.Models.Data;
+using NewsPortal.Models.Enums;
+using NewsPortal.Models.VeiwModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Domain.Builder
+{
+    public class NewsBuilder
+    {
+        private readonly INewsRepository _newsRepository;
+        private readonly INewsStorage _newsStorage;
+
+        public NewsBuilder(INewsRepository newsRepository, INewsStorage newsStorage)
+        {
+            _newsRepository = newsRepository;
+            _newsStorage = newsStorage;
+        }
+
+        public async Task<OperationResult> Add(NewsVM newsVM)
+        {
+            await _newsStorage.Add(newsVM);
+            return OperationResult.Success;
+        }
+
+        public async Task<NewsListVM> GetAll()
+        {
+            return await _newsStorage.GetAll();
+        }
+
+        public async Task<News> Get(Guid newsid)
+        {
+            return await _newsRepository.Get(newsid);
+        }
+
+        public async Task<OperationResult> Update(NewsVM newsVM)
+        {
+            if (!newsVM.NewsId.HasValue)
+            {
+                return OperationResult.InvalidId;
+            }
+
+            await _newsStorage.Update(newsVM);
+            return OperationResult.Success;
+        }
+
+    }
+}
---
+++ NewsPortal/Controllers/NewsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsPortal.Domain.Builder;
+using NewsPortal.Domain.Storage.Interfaces;
+using NewsPortal.Models.Data;
+using NewsPortal.Models.Enums;
+using NewsPortal.Models.VeiwModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NewsController :Controller
+    {
+        private readonly INewsStorage _newsStorage;
+        private readonly NewsBuilder _newsBuilder;
+
+        public NewsController(INewsStorage newsStorage, NewsBuilder newsBuilder)
+        {
+            _newsStorage = newsStorage;
+            _newsBuilder = newsBuilder;
+        }
+
+        /// <summary>
+        /// Метод на получение списка новостей
+        /// </summary>
+        /// <returns>Список всех новостей</returns>
+        [HttpGet("list/")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<NewsListVM> GetAll()
+        {
+            var news = await _newsBuilder.GetAll();
+            return news;
+        }
+
+        /// <summary>
+        /// Метод на получение информации по конкретной новости
+        /// </summary>
+        /// <param name="id">Уникальный идентификатор новости</param>
+        /// <returns>Модель искомой новости</returns>
+        [HttpGet("{newsId}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<News> Get([FromRoute]Guid newsId)
+        {
+            var news = await _newsBuilder.Get(newsId);
+            return news;
+        }
+
+        /// <summary>
+        /// Добавление новой новости
+        /// </summary>
+        /// <param name="NewsVM">Модель новой новости</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<OperationResult> Add([FromBody]NewsVM newsVM)
+        {
+            var result = await _newsBuilder.Add(newsVM);
+            return result;
+        }
+
+        /// <summary>
+        /// Метод на редактирование новости
+        /// </summary>
+        /// <param name="NewsVM">Изменённая модель новости</param>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task Put([FromBody]NewsVM newsVM)
+        {
+            await _newsBuilder.Update(newsVM);
+        }
+
+        /// <summary>
+        /// Метод на удаление новости
+        /// </summary>
+        /// <param name="newsId">Уникальный идентификатор новости</param>
+        /// <returns></returns>
+        [HttpDelete("{newsId}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task Delete([FromRoute]Guid newsId)
+        {
+            await _newsStorage.Delete(newsId);
+        }
+    }
+}
